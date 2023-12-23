73e2eddddee64d8f5e3404504461e87999ae5ea3
zzokku <93818329+zzokku@users.noreply.github.com>
2023-12-23 20:06:33 +0200

    snake and apple rendering

--- main.c
+++ main.c
@@ -1,46 +1,181 @@
 #include <stdio.h>
 #include "include/raylib.h"
 #include "include/raylib.h"
+#include <stdlib.h>
+#include <time.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 400
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 600
+#define SQUARE_SIZE 30
+#define APPLE_SIZE 30
+#define CENTER_X SCREEN_WIDTH / 2
+#define CENTER_Y SCREEN_HEIGHT / 2
+
+
+enum DIRS {
+    UP,
+    RIGHT,
+    LEFT,
+    DOWN
+}; 
+
+typedef struct snake
+{
+    int dir_x;
+    int dir_y;
+    struct body_part *head;
+
+} Snake;
+
+struct body_part {
+    struct body_part *next;
+    int x;
+    int y;
+};
+
+typedef struct apple {
+    int x;
+    int y;
+} Apple;
+
+
+void add_body_part(struct body_part *head){
+
+    struct body_part *new = (struct body_part*)malloc(sizeof(struct body_part));
+    new->x = head->x + SQUARE_SIZE;
+    new->y = head->y;
+
+    // finding last body_part
+    struct body_part *last = head;
+    while (last->next != NULL) {
+        last = last->next;
+    }
+
+    // linking last to new body_part
+    last->next = new;
+
+    return;
+}
+
+
+void render_snake(struct body_part *head){
+    struct body_part *p = head;
+    while (p != NULL) {
+        DrawRectangle(p->x, p->y, SQUARE_SIZE, SQUARE_SIZE, GREEN);
+        p = p->next;
+    }
+    return;
+}
+/* TODO
+void handle_input() {        
+    if (IsKeyDown(KEY_RIGHT)) 
+    if (IsKeyDown(KEY_LEFT))
+    if (IsKeyDown(KEY_UP)) 
+    if (IsKeyDown(KEY_DOWN))
+}
+
+*/
+
+// moves snake to direction provided by player
+void move_to_dir(int dir_x, int dir_y, Snake *snake){
+   
+    struct body_part *new_head = (struct body_part*)malloc(sizeof(struct body_part));
+    new_head->x = (snake->head)->x + SQUARE_SIZE * dir_x;
+    new_head->y = (snake->head)->y + SQUARE_SIZE * dir_y;
+    new_head->next = snake->head;
+    snake->head = new_head;
+    struct body_part *p = snake->head;
+    struct body_part *prev = NULL;
+    while (p->next != NULL) {
+        prev = p;
+        p = p->next;
+    }
+    if (prev != NULL) {
+        prev->next = NULL;
+        free(p);
+    }
+
+    return;
+}
+
+
+void render_apple(Apple apple){
+    DrawRectangle(apple.x, apple.y, APPLE_SIZE, APPLE_SIZE, RED);
+    return;
+}
+
+
+void init_snake(struct body_part *head){
+    for (int i = 1; i < 3; i++){
+        add_body_part(head);
+    }
+    return;
+}
+
+void check_for_apple(Apple *apple, Snake *snake){
+    if ((snake->head)->x == apple->x && (snake->head)->y == apple->y) {
+        apple->x = rand() % SCREEN_WIDTH - APPLE_SIZE;
+        apple->y = rand() % SCREEN_HEIGHT - APPLE_SIZE;
+        add_body_part(snake->head); 
+    }
+
+}
+
+
+
+
 
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    Snake snake;
+    snake.head = (struct body_part*)malloc(sizeof(struct body_part));
+    snake.dir_x = 1;
+    snake.dir_y = 0;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    Apple apple = {0};
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    srand((unsigned int)time(NULL));
+
+    apple.x = rand() % SCREEN_WIDTH - APPLE_SIZE;
+    apple.y = rand() % SCREEN_HEIGHT - APPLE_SIZE;
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "game");
+
+    snake.head->next= NULL;
+    snake.head->x = CENTER_X;
+    snake.head->y = CENTER_Y;
+
+
+    init_snake(snake.head);
+
+    SetTargetFPS(10);               
 
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
+
+    while (!WindowShouldClose()) {
+
+
         // Update
+        move_to_dir(snake.dir_x, snake.dir_y, &snake);
+
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
+        srand((unsigned int)time(NULL));
 
         // Draw
         //----------------------------------------------------------------------------------
+       
         BeginDrawing();
-
+            render_snake(snake.head);
+            render_apple(apple);
             ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+            
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
 
     return 0;
 }
